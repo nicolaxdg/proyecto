420a34886212120a9bc94ba124f3d5e120cb5588
JoseCarlosxdh <Jose.Merlanos@cecar.edu.co>
2023-06-06 04:28:27 -0500

    primer cambio

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <?php require 'partials/header.php' ?>
+
+    <?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+    <?php endif; ?>
+
+    <h1>Login</h1>
+    <span>or <a href="signup.php">SignUp</a></span>
+
+    <form action="login.php" method="POST">
+      <input name="correoUsuario" type="text" placeholder="Enter your email" >
+      <input name="contraUsuario" type="password" placeholder="Enter your Password" >
+      <input type="submit" value="Submit" >
+    </form>
+  </body>
+</html>
