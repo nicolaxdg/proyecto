a238b9fa189c1dcbb48f38c6f9d1e07b2f34f87c
CamiloGonzalez <camilo.gonzalezs@cecar.edu.co>
2023-06-06 03:28:40 -0500

    Primer cambio realizado

---
+++ signup.php
@@ -0,0 +1,29 @@
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>SignUp</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+
+    <?php require 'partials/header.php' ?>
+
+    <?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+    <?php endif; ?>
+
+    <h1>Registro</h1>
+    <span>o <a href="login.php">Ingresar</a></span>
+
+    <form action="signup.php" method="POST">
+      <input name="correoUsuario" type="text" placeholder="Ingrese su correo">
+      <input name="nombreUsuario" type="text" placeholder="Ingrese su nombre">
+      <input name="contraUsuario" type="password" placeholder="Ingrese su contraseña">
+      <input type="submit" value="Registrar">
+    </form>
+
+  </body>
+</html>
